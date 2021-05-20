851bdb8c5c1b56c7a81a54df4b8680c2a68ea344
Mostafa Mosbah <mmosbah195@gmail.com>
2021-05-19 21:24:23 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove(main);
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+victory.innerHTML = "<h1>Mosty is the champion</h1>"
